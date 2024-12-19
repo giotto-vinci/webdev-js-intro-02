aaaf47a667acb06b19cb04b3713bdd786eafd2e5
Roderick Fitzgerald <liber6schol@outlook.com>
2024-12-18 22:01:15 -0500

    final updates to readme, did some experiments in js index, and fixed rendering to make js assignment function properly

--- js/assignment.js
+++ js/assignment.js
@@ -2,29 +2,42 @@
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
-const yearElement = document.getElementById("year");
+const currentYearElement = document.getElementById("year");
 const resolutionElement = document.getElementById("resolution")
 const willMeetResolutionElement = document.getElementById("will-meet-resolution")
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
 
-function updateYear() {
-    // write the logic
+const currentYear = 2025;
+const resolution = "I will do enough work to take hold of an entry level front end development position, play music and excercise regularly, as well as sell artwork.";
 
+let willMeetResolution = "YES!";
+
+function updatecurrentYear() {
+    // write the logic
+    currentYearElement.innerText = currentYear
 }
 
 function updateResolution() {
     // Update this function 
+    resolutionElement.innerText = resolution
+}
+
+function updatewillMeetResolution() {
+
+    willMeetResolutionElement.innerText = willMeetResolution
 
 }
 
 function render() {
     // Finish writing this function
-
+    updatecurrentYear();
+    updateResolution();
+    updatewillMeetResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
     // Finish writing this function
-
+    render();
 })
--- js/index.js
+++ js/index.js
@@ -1,4 +1,41 @@
 // Prevent us from attempting to use variables
 // that are not declared
+/*
 "use strict"
 
+const myName = "Roderick";
+
+console.log(myName);
+
+console.log(typeof myName);
+
+const six = 6;
+
+console.log(six);
+
+console.log(typeof six);
+
+let isMentor = false;
+
+console.log(isMentor);
+
+console.log(typeof isMentor);
+
+isMentor = true;
+
+console.log(isMentor);
+
+console.log(typeof isMentor);
+
+let course = "WebDev";
+
+console.log(course);
+
+let x = "undefined";
+
+console.log(x);
+
+const obj = {};
+
+console.log(obj.nonExistentProperty);
+*/
